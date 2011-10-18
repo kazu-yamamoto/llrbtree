0e9a6ddb220302dd9cda3139dd7fa6fda22e2f0a
Kazu Yamamoto <kazu@iij.ad.jp>
2011-10-18 14:44:12 +0900

    removing noise.

--- bench/insert/Bench.hs
+++ bench/insert/Bench.hs
@@ -1,38 +1,38 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP, BangPatterns #-}
 
 module Main where
 
-import System.Random
 #if METHOD == 1
 import Data.RBTree
 #else
 import Data.RBTree.LL
 #endif
-import Progression.Main
-import Criterion.Main (bench, bgroup, nf)
 import Control.DeepSeq
+import Criterion.Main (bench, bgroup, nf)
 import Prelude hiding (lookup)
+import Progression.Main
+import System.Random
 
 seed :: Int
 seed = 54321
 
-ensure :: [Int] -> [Int]
-ensure xs = xs `deepseq` xs -- 'force' in the future
+ensure :: [Int] -> IO [Int]
+ensure xs = xs `deepseq` return xs
 
 genRandom :: Int -> [Int]
 genRandom n = take n . randoms . mkStdGen $ seed
 
 main :: IO ()
 main = do
-    let !i1 = ensure [1..  10000]
-        !i2 = ensure [1.. 100000]
-        !i3 = ensure [1..1000000]
-        !d1 = ensure [  10000,  9999..1]
-        !d2 = ensure [ 100000, 99999..1]
-        !d3 = ensure [1000000,999999..1]
-        !r1 = ensure $ genRandom   10000
-        !r2 = ensure $ genRandom  100000
-        !r3 = ensure $ genRandom 1000000
+    !i1 <- ensure [1..  10000]
+    !i2 <- ensure [1.. 100000]
+    !i3 <- ensure [1..1000000]
+    !d1 <- ensure [  10000,  9999..1]
+    !d2 <- ensure [ 100000, 99999..1]
+    !d3 <- ensure [1000000,999999..1]
+    !r1 <- ensure $ genRandom   10000
+    !r2 <- ensure $ genRandom  100000
+    !r3 <- ensure $ genRandom 1000000
     defaultMain $
         bgroup "" [
              bench "inc 10^4" $ nf fromList i1

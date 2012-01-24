9c58a4ccc60c35490ab455222271a751366fdea8
Kazu Yamamoto <kazu@iij.ad.jp>
2012-01-24 18:02:55 +0900

    adding tests and doc fix.

--- Data/Set/Splay.hs
+++ Data/Set/Splay.hs
@@ -1,4 +1,245 @@
-module Data.Set.Splay where
+{-|
+  Purely functional splay trees.
+
+   * <http://www.cs.cmu.edu/~sleator/papers/self-adjusting.pdf>
+-}
+
+module Data.Set.Splay (
+  -- * Data structures
+    Splay(..)
+  -- * Creating red-black trees
+  , empty
+  , singleton
+  , insert
+  , fromList
+  -- * Converting a list
+  , toList
+  -- * Membership
+  , member
+  -- * Deleting
+--  , delete
+  , deleteMin
+--  , deleteMax
+  -- * Checking
+  , null
+  -- * Set operations
+  , union
+--  , intersection
+--  , difference
+  -- * Helper functions
+  , partition
+--  , merge
+  , minimum
+--  , maximum
+  , valid
+  , showTree
+  , printTree
+  ) where
+
+import Data.List (foldl')
+import Prelude hiding (minimum, maximum, null)
+
+----------------------------------------------------------------
+
+data Splay a = Leaf | Node (Splay a) a (Splay a) deriving Show
+
+instance (Eq a) => Eq (Splay a) where
+    t1 == t2 = toList t1 == toList t2
+
+----------------------------------------------------------------
+
+{-| Splitting smaller and bigger with splay.
+    Since this is a set implementation, members must be unique.
+-}
+partition :: Ord a => a -> Splay a -> (Splay a, Bool, Splay a)
+partition _ Leaf = (Leaf,False,Leaf)
+partition k x@(Node xl xk xr) = case compare k xk of
+    EQ -> (xl, True, xr)
+    GT -> case xr of
+        Leaf -> (x, False, Leaf)
+        Node yl yk yr -> case compare k yk of
+            EQ ->     (Node xl xk yl, True, yr)           -- R  :zig
+            GT -> let (lt, b, gt) = partition k yr        -- RR :zig zag
+                  in  (Node (Node xl xk yl) yk lt, b, gt)
+            LT -> let (lt, b, gt) = partition k yl
+                  in  (Node xl xk lt, b, Node gt yk yr)   -- RL :zig zig
+    LT -> case xl of
+        Leaf          -> (Leaf, False, x)
+        Node yl yk yr -> case compare k yk of
+            EQ ->     (yl, True, Node yr xk xr)           -- L  :zig
+            GT -> let (lt, b, gt) = partition k yr        -- LR :zig zag
+                  in  (Node yl yk lt, b, Node gt xk xr)
+            LT -> let (lt, b, gt) = partition k yl        -- LL :zig zig
+                  in  (lt, b, Node gt yk (Node yr xk xr))
+
+----------------------------------------------------------------
+
+{-| Empty tree.
+-}
+
+empty :: Splay a
+empty = Leaf
+
+{-|
+See if the splay tree is empty.
+
+>>> Data.Set.Splay.null empty
+True
+>>> Data.Set.Splay.null (singleton 1)
+False
+-}
+
+null :: Splay a -> Bool
+null Leaf = True
+null _ = False
+
+{-| Singleton tree.
+-}
+
+singleton :: a -> Splay a
+singleton x = Node Leaf x Leaf
+
+----------------------------------------------------------------
+
+{-| Insertion.
+
+>>> insert 5 (fromList [5,3]) == fromList [3,5]
+True
+>>> insert 7 (fromList [5,3]) == fromList [3,5,7]
+True
+>>> insert 5 empty            == singleton 5
+True
+-}
+
+insert :: Ord a => a -> Splay a -> Splay a
+insert x t = Node l x r
+  where
+    (l,_,r) = partition x t
+
+----------------------------------------------------------------
+
+{-| Creating a tree from a list.
+
+>>> empty == fromList []
+True
+>>> singleton 'a' == fromList ['a']
+True
+>>> fromList [5,3,5] == fromList [5,3]
+True
+-}
+
+fromList :: Ord a => [a] -> Splay a
+fromList = foldl' (flip insert) empty
+
+----------------------------------------------------------------
+
+{-| Creating a list from a tree. O(N)
+
+>>> toList (fromList [5,3])
+[3,5]
+>>> toList empty
+[]
+-}
+
+toList :: Splay a -> [a]
+toList t = inorder t []
+  where
+    inorder Leaf xs = xs
+    inorder (Node l x r) xs = inorder l (x : inorder r xs)
+
+----------------------------------------------------------------
+
+{-| Checking if this element is a member of a tree?
+
+>>> fst $ member 5 (fromList [5,3])
+True
+>>> fst $ member 1 (fromList [5,3])
+False
+-}
+
+member :: Ord a => a -> Splay a -> (Bool, Splay a)
+member x t = if b then
+                 (True, Node l x r)
+             else
+                 (False, t)
+  where
+    (l,b,r) = partition x t
+
+----------------------------------------------------------------
+
+{-| Finding the minimum element.
+
+>>> fst $ minimum (fromList [3,5,1])
+1
+>>> minimum empty
+*** Exception: minimum
+-}
+
+minimum :: Splay a -> (a, Splay a)
+minimum Leaf = error "minimum"
+minimum t = let (x,mt) = deleteMin t in (x, Node Leaf x mt)
+
+----------------------------------------------------------------
+
+{-| Deleting the minimum element.
+
+>>> snd (deleteMin (fromList [5,3,7])) == fromList [5,7]
+True
+>>> deleteMin empty
+*** Exception: deleteMin
+-}
+
+deleteMin :: Splay a -> (a, Splay a)
+deleteMin Leaf = error "deleteMin"
+deleteMin (Node Leaf x r)            = (x,r)
+deleteMin (Node (Node Leaf lx lr) x r)  = (lx, Node lr x r)
+deleteMin (Node (Node ll lx lr) x r) = let (k,mt) = deleteMin ll
+                                 in (k, Node mt lx (Node lr x r))
+
+----------------------------------------------------------------
+{-| Creating a union tree from two trees.
+
+>>> union (fromList [5,3]) (fromList [5,7]) == fromList [3,5,7]
+True
+-}
+
+union :: Ord a => Splay a -> Splay a -> Splay a
+union Leaf t = t
+union (Node a x b) t = Node (union ta a) x (union tb b)
+  where
+    (ta,_,tb) = partition x t
+
+----------------------------------------------------------------
+-- Basic operations
+----------------------------------------------------------------
+
+{-| Checking validity of a tree.
+-}
+
+valid :: Ord a => Splay a -> Bool
+valid t = isOrdered t
+
+isOrdered :: Ord a => Splay a -> Bool
+isOrdered t = ordered $ toList t
+  where
+    ordered [] = True
+    ordered [_] = True
+    ordered (x:y:xys) = x < y && ordered (y:xys)
+
+
+showTree :: Show a => Splay a -> String
+showTree = showTree' ""
+
+showTree' :: Show a => String -> Splay a -> String
+showTree' _ Leaf = "\n"
+showTree' pref (Node l x r) = show x ++ "\n"
+                        ++ pref ++ "+ " ++ showTree' pref' l
+                        ++ pref ++ "+ " ++ showTree' pref' r
+  where
+    pref' = "  " ++ pref
+
+printTree :: Show a => Splay a -> IO ()
+printTree = putStr . showTree
 
 {-
 Demo: http://www.link.cs.cmu.edu/splay/
@@ -14,94 +255,3 @@ Skew Heap      N log N     1                log N(???)      N/A
 Splay Heap     N           log N or A(N)?   log N or A(N)?  log N or A(N)?
 
 -}
-
-import Data.List (foldl')
-import Prelude hiding (minimum)
-
-data Splay a = E | T (Splay a) a (Splay a)
-
-instance Show a => Show (Splay a) where
-    show = showTree
-
-partition :: Ord a => a -> Splay a -> (Splay a, Bool, Splay a)
-partition _ E = (E,False,E)
-partition k x@(T xl xk xr) = case compare k xk of
-    EQ -> (xl, True, xr)
-    GT -> case xr of
-        E -> (x, False, E)
-        T yl yk yr -> case compare k yk of
-            EQ ->     (T xl xk yl, True, yr)        -- right        :zig
-            GT -> let (lt, b, gt) = partition k yr  -- right right  :zig zag
-                  in  (T (T xl xk yl) yk lt, b, gt)
-            LT -> let (lt, b, gt) = partition k yl
-                  in  (T xl xk lt, b, T gt yk yr)   -- right left   :zig zig
-    LT -> case xl of
-        E          -> (E, False, x)
-        T yl yk yr -> case compare k yk of
-            EQ ->     (yl, True, T yr xk xr)        -- left         :zig
-            GT -> let (lt, b, gt) = partition k yr  -- left right   :zig zag
-                  in  (T yl yk lt, b, T gt xk xr)
-            LT -> let (lt, b, gt) = partition k yl  -- left left    :zig zig
-                  in  (lt, b, T gt yk (T yr xk xr))
-
-empty :: Splay a
-empty = E
-
-null :: Splay a -> Bool
-null E = True
-null _ = False
-
-singleton :: a -> Splay a
-singleton x = T E x E
-
-insert :: Ord a => a -> Splay a -> Splay a
-insert x t = T l x r
-  where
-    (l,_,r) = partition x t
-
-fromList :: Ord a => [a] -> Splay a
-fromList = foldl' (flip insert) empty
-
-member :: Ord a => a -> Splay a -> (Bool, Splay a)
-member x t = if b then
-                 (True, T l x r)
-             else
-                 (False, t)
-  where
-    (l,b,r) = partition x t
-    
-{-
-  let t = fromList xs
-  minimum (fromList xs) == let m = Data.List.minimum xs
-                               (_, t') = member m t
-                           in (m, t')
--}
-minimum :: (Ord a, Bounded a) => Splay a -> (a, Splay a)
-minimum t = let (x,mt) = deleteMin t in (x, T E x mt)
-
-deleteMin :: Splay a -> (a, Splay a)
-deleteMin E = error "deleteMin"
-deleteMin (T E x r)            = (x,r)
-deleteMin (T (T E lx lr) x r)  = (lx, T lr x r)
-deleteMin (T (T ll lx lr) x r) = let (k,mt) = deleteMin ll
-                                 in (k, T mt lx (T lr x r))
-
-merge :: Ord a => Splay a -> Splay a -> Splay a
-merge E t = t
-merge (T a x b) t = T (merge ta a) x (merge tb b)
-  where
-    (ta,_,tb) = partition x t
-
-showTree :: Show a => Splay a -> String
-showTree = showTree' ""
-
-showTree' :: Show a => String -> Splay a -> String
-showTree' _ E = "\n"
-showTree' pref (T l x r) = show x ++ "\n"
-                        ++ pref ++ "+ " ++ showTree' pref' l
-                        ++ pref ++ "+ " ++ showTree' pref' r
-  where
-    pref' = "  " ++ pref
-
-printTree :: Show a => Splay a -> IO ()
-printTree = putStr . showTree
